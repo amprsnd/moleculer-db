7ac53b7c179cd46d40d4c51192c0b453407c1e61
Nikolay <ampersand@pseudostudio.info>
2020-03-11 14:49:19 +0600

    Update index.js

--- packages/moleculer-db-adapter-mongoose/src/index.js
+++ packages/moleculer-db-adapter-mongoose/src/index.js
@@ -14,6 +14,7 @@ const mongoose  = require("mongoose");
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useFindAndModify", false);
 mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
 
 class MongooseDbAdapter {
 
